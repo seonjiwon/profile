25137e25721ccd2c61347557216989a38af9ff17
seonjiwon <tommys915@gmail.com>
2026-04-08 11:55:35 +0900

    feat: 배포 준비 완료

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
   // VM 또는 다른 호스트에서 접속 허용
   allowedDevOrigins: ['172.21.26.129', '*'],
   // 정적 빌드 설정
-  output: 'standalone',
+  output: 'export',
 };
 
 export default nextConfig;
